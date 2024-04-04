a6c816dbe96988870fd8534e1a48dddf89e95437
carrie627 <hossaintanvir153@gmail.com>
2024-04-04 11:45:28 +0600

    Implement Dijktra's Algorithm in both unoptimized and optimized way with two extra files named DijkstrasFirstVersion.js and DijkstraBinaryHeapVersion.js

--- 28-dijkstras-algorithm/DijkstraBinaryHeapVersion.js
+++ 28-dijkstras-algorithm/DijkstraBinaryHeapVersion.js
@@ -1,21 +1,4 @@
-class PriorityQueue {
-    constructor() {
-        this.values = [];
-    }
-
-    enqueue(value, priority) {
-        this.values.push({ value, priority });
-        this.sort();
-    }
-
-    dequeue() {
-        return this.values.shift();
-    }
-
-    sort() {
-        this.values.sort((a, b) => a.priority - b.priority);
-    }
-}
+// THIS THE MODIFIED AND OPTIMIZED VERSION OF DIJKSTRA'S ALGORITHM
 
 class WeightedGraph {
     constructor() {
@@ -91,6 +74,87 @@ class WeightedGraph {
     }
 }
 
+class Node {
+    constructor(value, priority) {
+        this.value = value;
+        this.priority = priority;
+    }
+}
+
+class PriorityQueue {
+    constructor() {
+        this.values = [];
+    }
+
+    dequeue() {
+        let min = this.values[0];
+        let end = this.values.pop();
+        if (this.values.length > 0) {
+            this.values[0] = end;
+            this._sinkDown();
+        }
+        return min;
+    }
+
+    enqueue(value, priority) {
+        let newNode = new Node(value, priority);
+        this.values.push(newNode);
+        this._bubbleUp();
+    }
+
+    _bubbleUp() {
+        let idx = this.values.length - 1;
+        let element = this.values[idx];
+
+        while (idx > 0) {
+            let parentIdx = Math.floor((idx - 1) / 2);
+            let parent = this.values[parentIdx];
+
+            if (element.priority >= parent.priority) break;
+            this.values[parentIdx] = element;
+            this.values[idx] = parent;
+            idx = parentIdx;
+        }
+    }
+
+    _sinkDown() {
+        let idx = 0;
+        const length = this.values.length;
+        const element = this.values[0];
+
+        while (true) {
+            let leftChildIdx = 2 * idx + 1;
+            let rightChildIdx = 2 * idx + 2;
+            let leftChild, rightChild;
+            let swap = null;
+
+            if (leftChildIdx < length) {
+                leftChild = this.values[leftChildIdx];
+
+                if (leftChild.priority < element.priority) {
+                    swap = leftChildIdx;
+                }
+            }
+
+            if (rightChildIdx < length) {
+                rightChild = this.values[rightChildIdx];
+
+                if (
+                    (swap === null && rightChild.priority < element.priority) ||
+                    (swap !== null && rightChild.priority < leftChild.priority)
+                ) {
+                    swap = rightChildIdx;
+                }
+            }
+
+            if (swap === null) break;
+            this.values[idx] = this.values[swap];
+            this.values[swap] = element;
+            idx = swap;
+        }
+    }
+}
+
 let graph = new WeightedGraph();
 graph.addVertex("A");
 graph.addVertex("B");
@@ -108,5 +172,5 @@ graph.addEdge("D", "E", 3);
 graph.addEdge("D", "F", 1);
 graph.addEdge("E", "F", 1);
 
-let a = graph.Dijkstra("A", "E");
+let a = graph.Dijkstra("A", "F");
 console.log(a);
